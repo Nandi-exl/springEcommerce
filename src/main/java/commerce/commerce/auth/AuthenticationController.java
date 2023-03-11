7483faf3a5951d28aafff8fd67ae9ecc9c248313
Nandi Naulfaldy <nandinaufaldi@gmail.com>
2023-03-11 15:00:25 +0700

    'add_new_http_response'

--- src/main/java/commerce/commerce/auth/AuthenticationController.java
+++ src/main/java/commerce/commerce/auth/AuthenticationController.java
@@ -1,21 +1,22 @@
 package commerce.commerce.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationService service;
 
     @PostMapping("/login")
-    public ResponseEntity<Authentication> authenticate(
+    public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
             ){
-        return ResponseEntity.ok(service.authenticate(request))
+        return ResponseEntity.ok(service.authenticate(request));
     }
 
 }
---
+++ src/main/java/commerce/commerce/auth/AuthenticationService.java
@@ -0,0 +1,21 @@
+package commerce.commerce.auth;
+
+import commerce.commerce.config.JwtService;
+import commerce.commerce.repository.UserRepository;
+import commerce.commerce.token.TokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+
+}
